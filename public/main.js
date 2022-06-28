1d6dcd171ede6c0b17cac229c36e54e0427791d8
Seth White <seth.white@strakertranslations.com>
2022-06-28 10:41:44 -0400

    Figured refs out

--- public/main.js
+++ public/main.js
@@ -1,7 +1,9 @@
 import { getEvents, eventsChanged } from "/store/localEvents.js";
 import v from "/vvv";
-const { div, button } = v;
+const { div, button, output } = v;
 import { EventList } from "/components/EventList.js";
+
+let message;
 
 div({
 	parentElement: document.body,
@@ -11,6 +13,9 @@ div({
 			textContent: "Refresh",
 			click: getEvents,
 		}),
+		(message = output()),
 		EventList(eventsChanged),
 	],
 });
+
+eventsChanged((events) => (message.value = `Got ${events.length} events`));
--- public/store/localEvents.js
+++ public/store/localEvents.js
@@ -1,22 +1,21 @@
 import { events } from "/vvv";
 const data = {
-  events: [],
+	events: [],
 };
 
 const gotEventsKey = events.createEventKey("gotEvents");
 
 export const eventsChanged = (callback) => {
-  events.addEventHandler(gotEventsKey, callback);
-  return callback(data.events);
+	events.addEventHandler(gotEventsKey, callback);
+	return callback(data.events);
 };
 
 export const getEvents = async () => {
-  const response = await fetch("/events");
-  if (!response.ok) {
-    throw new Error(`Could not fetch events`);
-  }
-  const events = await response.json();
-  data.events = events;
-  events.dispatchEvent(gotEventsKey, data.events);
-  return events;
+	const response = await fetch("/events");
+	if (!response.ok) {
+		throw new Error(`Could not fetch events`);
+	}
+	data.events = await response.json();
+	events.dispatchEvent(gotEventsKey, data.events);
+	return data.events;
 };
